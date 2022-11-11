69e9cb5c51d3e05a4448e0172e1eecdd70422ff3
sameishchev.anton <anton.sameishchev@devexpress.com>
2022-11-11 16:22:04 +0400

    MAUI Code added

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Maui.DataGrid;
+using DevExpress.Maui.Core;
+
+namespace maui_grid_get_started_migrated {
+    public partial class MainPage : ContentPage {
+        int count;
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        void grid_CalculateCustomSummary(System.Object sender, DevExpress.Maui.DataGrid.CustomSummaryEventArgs e) {
+            if (e.FieldName.ToString() == "Shipped")
+                if (e.IsTotalSummary) {
+                    if (e.SummaryProcess == DataSummaryProcess.Start) {
+                        count = 0;
+                    }
+                    if (e.SummaryProcess == DataSummaryProcess.Calculate) {
+                        if (!(bool)e.FieldValue)
+                            count++;
+                        e.TotalValue = count;
+                    }
+                }
+        }
+    }
+}
