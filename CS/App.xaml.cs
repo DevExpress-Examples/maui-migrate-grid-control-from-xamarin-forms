600699b298042486872c000244d9ecdfc5dd9dd1
sameishchev.anton <anton.sameishchev@devexpress.com>
2022-11-11 16:51:16 +0400

    MAUI Code Added

---
+++ CS/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace maui_grid_get_started_migrated;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+}
